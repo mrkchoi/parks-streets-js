1cc02793ad62ed1d6e36cab9cfb8014b4d7a0abb
Kenny <kennethichoi@gmail.com>
2018-11-22 21:59:39 -0800

    ES6 subclass refactor v2

--- scripts/app.js
+++ scripts/app.js
@@ -18,22 +18,27 @@ At an end-of-year meeting, your boss wants a final report with the following:
 
 All the report data should be printed to the console.
 
-HINT: Use some of the ES6 features: classes, subclasses, template strings, default parameters, maps, arrow functions, destructuring, etc.
 
 */
-// 3 parks total
-class Park {
-    constructor(name, buildYear, numTrees, parkArea) {
+
+class Element {
+    constructor(name, buildYear) {
         this.name = name;
         this.buildYear = buildYear;
+    }
+}
+
+// 3 parks total
+class Park extends Element {
+    constructor(name, buildYear, numTrees, parkArea) {
+        super(name, buildYear);
         this.numTrees = numTrees;
         this.parkArea = parkArea;
     }
 
     treeDensity() {
         let treeDensity = parseFloat(this.numTrees / this.parkArea).toFixed(2);
-        // console.log(`${this.name} has a tree density of ${treeDensity} trees per square km`);
-        return treeDensity;
+        console.log(`${this.name} has a tree density of ${treeDensity} trees per square km`);
     }
     parkAge() {
         let age = new Date().getFullYear() - this.buildYear;
@@ -42,107 +47,77 @@ class Park {
 }
 
 // 4 streets total
-class Street {
-    constructor(name, buildYear, length) {
-        this.name = name;
-        this.buildYear = buildYear;
+class Street extends Element {
+    constructor(name, buildYear, length, size = 3) {
+        super(name, buildYear);
         this.length = length;
+        this.size = size;
     }
 
-    size() {
-        let size;
-        if(this.length > 10) {
-            size = 'huge';
-        } else if(this.length > 7.5) {
-            size = 'big';
-        } else if(this.length > 5) {
-            size = 'normal';
-        } else if(this.length > 2.5) {
-            size = 'small';
-        } else if(this.length > 1){
-            size = 'tiny';
-        }
-        return size;
+    classifyStreet() {
+        const classification = new Map();
+        classification.set(1, 'tiny');
+        classification.set(2, 'small');
+        classification.set(3, 'normal');
+        classification.set(4, 'big');
+        classification.set(5, 'huge');
+        console.log(`${this.name}, built in ${this.buildYear}, is a ${classification.get(this.size)} street`);
     }
 }
 
 // Parks
-let greenPark = new Park('Green Park', 1991, 173, 2.7);
-let natPark = new Park('National Park', 1967, 3351, 6.8);
-let oakPark = new Park('Oak Park', 2007, 496, 1.4);
+let allParks = [new Park('Green Park', 1991, 173, 2.7),
+                new Park('National Park', 1967, 3351, 6.8),
+                new Park('Oak Park', 2007, 496, 1.4)];
 
 // Streets
-let oceanAve = new Street('Ocean Avenue', 1999, 7.8);
-let evergreenStreet = new Street('Evergreen Street', 2008, 2.6);
-let fourthStreet = new Street('4th Street', 2015, 5.3);
-let sunsetBlvd = new Street('Sunset Boulevard', 1982, 12.2);
+let allStreets = [  new Street('Ocean Avenue', 1999, 7.8, 4),
+                    new Street('Evergreen Street', 2008, 2.6, 2), 
+                    new Street('4th Street', 2015, 5.3),
+                    new Street('Sunset Boulevard', 1982, 12.2, 5)];
 
-// Data
-let totalParks = [greenPark, natPark, oakPark];
-let totalStreets = [oceanAve, evergreenStreet, fourthStreet, sunsetBlvd];
 
-// 2. Average age of each town's park (forumla: sum of all ages/number of parks)
-let parkAvgAge = function() {
-    let totalYears = 0;
-
-    for(let park of totalParks) {
-        totalYears += park.parkAge();
-    }
-    // console.log(totalYears);
-    
-    // Divide parkArea by # of parks
-    let avgAge = parseFloat(totalYears / (totalParks.length)).toFixed(2);
-    
-    // return the output
-    return avgAge;
+// Calc function
+function calc(arr) {
+    const sum = arr.reduce((acc, cur, index) => {
+        return acc + cur;
+    }, 0);
+    return [sum, sum / arr.length];
 }
 
-// 3. The name of the park that has more than 1000 trees
-let maxTrees = function() {
-    // Iterate through parks and check the number of trees
-    for(let park of totalParks) {
-        // If number of trees is > 1000, return the name of the park
-        if(park.numTrees > 1000) {
-            return park.name;
-        }
-    }
-}
-
-// 4. Total and average length of the town's streets
-let streetLength = function() {
-    let lengthTotal = 0;
-    let avgLength;
-
-    for(let street of totalStreets) {
-        lengthTotal += street.length;
-    }
-
-    avgLength = lengthTotal / totalStreets.length;
-    
-    return [lengthTotal, avgLength];
-}
-
-let displayData = function () {
+function reportParks(p) {
     // PARKS - Report header
     console.log('----PARKS REPORT----');
-    // PARKS - Avg age
-    console.log(`Our ${totalParks.length} parks have an average age of ${parkAvgAge()} years.`);
-    // PARKS - Tree density report
-    for(let park of totalParks) {
-        console.log(`${park.name} has a tree density of ${park.treeDensity()} tress per square km`);
-    }
-    // PARKS - Highest tree density
-    console.log(`${maxTrees()} has more than 1000 trees.`);
 
+    // Run tree density report
+    p.forEach(el => el.treeDensity());
+
+    // Average Age
+    const ages = p.map(el => new Date().getFullYear() - el.buildYear);
+    // console.log(ages);
+
+    const [totalYears, AvgAge] = calc(ages);
+    console.log(`Our ${ages.length} parks have an average of ${AvgAge} years.`);
+
+    // Which park has more than 1000 trees
+    const i = p.map(el => {
+        return el.numTrees;
+    }).findIndex(el => el > 1000);
+    console.log(`${p[i].name} has more than 1000 trees.`);
+
+}
+
+function reportStreets(s) {
     // STREETS - Report header
-    console.log('----STREETS REPORT----');
-    // STREETS - Total & Avg lengths
-    console.log(`Our ${totalStreets.length} streets have a total length of ${streetLength()[0]} km, with an average of ${streetLength()[1]} km.`);
-    // STREET - Size classifications
-    for(let street of totalStreets) {
-        console.log(`${street.name}, built in ${street.buildYear}, is a ${street.size()} street`);
-    }
+    console.log('----STREET REPORT----');
 
+    // Total and average length of town's streets
+    const [totalLength, avgLength] = calc(s.map(el => el.length));
+    console.log(`Our ${s.length} streets have a total length of ${totalLength} km, with an average of ${avgLength} km.`);
 
-};
-displayData();
+    // Classify sizes
+    s.forEach(el => el.classifyStreet());
+}
+    
+reportParks(allParks);
+reportStreets(allStreets);
